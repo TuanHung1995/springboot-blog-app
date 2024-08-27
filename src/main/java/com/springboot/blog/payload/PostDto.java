9218f44d2a1df1505f9a6567e841c54d1ae0e0db
TuanHung1995 <tuanhungg1995@gmail.com>
2024-08-27 10:17:47 +0700

    add validation for post and comment request

--- src/main/java/com/springboot/blog/payload/PostDto.java
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -1,6 +1,8 @@
 package com.springboot.blog.payload;
 
 import com.springboot.blog.entity.Comment;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.Set;
@@ -9,8 +11,16 @@ import java.util.Set;
 public class PostDto {
 
     private Long id;
+
+    @NotEmpty
+    @Size(min = 2, message = "Post title must be at least 2 characters long")
     private String title;
+
+    @NotEmpty
+    @Size(min = 10, message = "Post description must be at least 10 characters long")
     private String description;
+
+    @NotEmpty
     private String content;
     private Set<Comment> comments;
     
