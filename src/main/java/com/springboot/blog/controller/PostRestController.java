91e94e7647d034b15dd471d421299e850e32e7cc
TuanHung1995 <tuanhungg1995@gmail.com>
2024-09-04 14:53:02 +0700

    versioning rest api

--- src/main/java/com/springboot/blog/controller/PostRestController.java
+++ src/main/java/com/springboot/blog/controller/PostRestController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping("/api/v1/posts")
 public class PostRestController {
 
     private final PostService postService;
